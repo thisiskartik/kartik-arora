14142704589382f4c9ced9fc12c4854caccb0e50
Kartik Arora <kartikcsengineer@gmail.com>
2021-12-13 15:09:43 +0530

    Created React App for the website and moved the coming soon page to React.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+	return (
+		<main>
+			<h1>Coming Soon!</h1>
+			<p>Thanks for visiting kartik-arora.com We're going to be live soon.</p>
+		</main>
+	);
+};
+
+export default App;
